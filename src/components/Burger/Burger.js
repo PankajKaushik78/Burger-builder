d348357927a11c52e668bc7368c1dc0fefecd4b0
PankajKaushik78 <kaushikpankaj78@gmail.com>
2021-01-17 13:57:54 +0530

    Added burger and burger control ui

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,25 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredient => {
+      return [...Array(props.ingredients[ingredient])]
+        .map((_,idx) => {
+          return <BurgerIngredient key={ingredient+idx} type={ingredient} />
+        });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding some ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="salad" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
